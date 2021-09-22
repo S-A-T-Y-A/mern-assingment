6a339dd866e2939157e5467bfc1e6264dd93fb96
satya <github846@gmail.com>
2021-09-22 20:54:36 +0530

    js file added

--- popup_login_form/app.js
+++ popup_login_form/app.js
@@ -1,1 +1,7 @@
 
+const btn=document.querySelector(".button");
+const form=document.querySelector(".card");
+form.style.display="none";
+btn.addEventListener("click",()=>{
+    form.classList.toggle("card_show");
+})
